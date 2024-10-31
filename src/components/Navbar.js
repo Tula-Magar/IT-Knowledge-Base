f0966c6c016874e2292bca6d42f60b8686573053
Tulay Magar <tmagar@nhpd.org>
2024-10-31 13:58:44 -0400

    collapse after clicking on link and scroll to top

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import { Link, useLocation } from 'react-router-dom';
 import "./Navbar.css";
 
 function Navbar() {
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState('');
   const location = useLocation();
 
@@ -16,34 +17,42 @@ function Navbar() {
     console.log(searchTerm); // Just for demonstration
   };
 
+  const toggleNavbar = () => {
+    setIsNavbarOpen(!isNavbarOpen);
+  };
+
+  const closeNavbar = () => {
+    setIsNavbarOpen(false);
+    window.scrollTo(0, 0); // Scroll to the top of the page
+  };
+
   return (
-    <nav className="navbar navbar-expand-lg sticky-top" >
-      <div className="container-fluid" style={{paddingLeft:"10px", marginRight:"10px"}} >
-        <Link className="navbar-brand text-light smaller" to="/">MyApp</Link>
+    <nav className="navbar navbar-expand-lg sticky-top">
+      <div className="container-fluid" style={{ paddingLeft: "10px", marginRight: "10px" }}>
+        <Link className="navbar-brand text-light smaller" to="/" onClick={closeNavbar}>MyApp</Link>
         <button
           className="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
+          onClick={toggleNavbar}
           aria-controls="navbarNav"
-          aria-expanded="false"
+          aria-expanded={isNavbarOpen}
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className={`collapse navbar-collapse ${isNavbarOpen ? 'show' : ''}`} id="navbarNav">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link text-light" to="/">Home</Link>
+              <Link className="nav-link text-light" to="/" onClick={closeNavbar}>Home</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-light" to="/about">About</Link>
+              <Link className="nav-link text-light" to="/about" onClick={closeNavbar}>About</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-light" to="/contact">Contact</Link>
+              <Link className="nav-link text-light" to="/contact" onClick={closeNavbar}>Contact</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-light" to="/content">Content</Link>
+              <Link className="nav-link text-light" to="/content" onClick={closeNavbar}>Content</Link>
             </li>
           </ul>
           {/* Conditionally render the search form only on the /content page */}
